554481b953db5643b802c20822ef87b9b20214ec
MrPinguino <33909104+MrPinguino@users.noreply.github.com>
2018-01-17 21:23:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    const m = await message.channel.send("Ping?")
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`)
+    message.author.reply('sdfda')
   	}
 });
 
