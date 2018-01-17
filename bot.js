30671b99b228d689d3aa954e745f8e330a608773
MrPinguino <33909104+MrPinguino@users.noreply.github.com>
2018-01-17 20:58:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-        message.delete();
+        message.delete(1000);
     	message.reply('pong');
   	}
 });
