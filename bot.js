b0a59c487d7770e2b28462569c1528e8c751c097
MrPinguino <33909104+MrPinguino@users.noreply.github.com>
2018-01-18 18:33:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('Last Warn or you will get Banned!');
+    	message.reply('Last Warn or you will get **Banned!**');
   	}
 });
 
